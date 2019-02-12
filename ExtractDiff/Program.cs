1e000e3b58d5c4f04673dc61cc8572bb53dc9efd
mikkelhm <mikkel@umbraco.com>
2019-02-12 08:09:58 +0100

    initial commit

---
+++ ExtractDiff/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExtractDiff
+{
+    class Program
+    {
+        private static string workingDirectory;
+        // UmbracoCms, UmbracoDeploy, UmbracoForms
+        private static string packageName;
+        // UmbracoCms.7.12.2, UmbracoDeploy.v2.0.15, UmbracoForms.Files.7.0.6 - woohoo for consistency
+        private static string packageNameVersionIndexOf;
+        private static ZipService _zipService;
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine($"I need to be invoked with 4 args, workingDirectory, packageName, newVersion");
+                return;
+            }
+
+            workingDirectory = args[0];
+            packageName = args[1];
+            //var oldPackagePath = args[2];
+            var newPackageVersion = args[2];
+            if (Version.TryParse(newPackageVersion, out var newVersion) == false)
+            {
+                newVersion = new Version(GetVersionFromPackage(newPackageVersion));
+            }
+            if (newVersion.Build == 0)
+            {
+                Console.WriteLine("New version is the first release of this minor, diffs are only done between patches");
+                return;
+            }
+            Version oldVersion;
+            if (args.Length > 3)
+            {
+                oldVersion = new Version(args[3]);
+            }
+            else
+            {
+                oldVersion = new Version(newVersion.Major, newVersion.Minor, newVersion.Build - 1);
+            }
+            if (args.Length > 4)
+            {
+                packageNameVersionIndexOf = args[4];
+            }
+            else
+            {
+                packageNameVersionIndexOf = packageName + ".";
+            }
+            Console.WriteLine($"Running with values workingDirectory {workingDirectory}, packageName: {packageName}, newPackageVersion: {newPackageVersion}, packageNameVersionIndexOf: {packageNameVersionIndexOf}");
+
+            // Cough cough: "Dependency Injection"?
+            _zipService = new ZipService();
+            var umbracoPackageDownloader = new PackageDownloader("https://umbracoreleases.blob.core.windows.net/download/UmbracoCms.{version}.zip");
+
+            var oldPackagePath =
+                Path.Combine(workingDirectory, packageNameVersionIndexOf + oldVersion.ToString(3) + ".zip");
+            var newPackagePath = Path.Combine(workingDirectory, packageNameVersionIndexOf + newVersion.ToString(3) + ".zip");
+            umbracoPackageDownloader.EnsurePackage(packageName, oldVersion, oldPackagePath);
+            umbracoPackageDownloader.EnsurePackage(packageName, newVersion, newPackagePath);
+            Extract(oldPackagePath);
+            Extract(newPackagePath);
+
+            CompareVersions(oldPackagePath, newPackagePath);
+        }
+
+        private static void CompareVersions(string oldPackagePath, string newPackagePath)
+        {
+            var oldVersion = GetVersionFromPackage(oldPackagePath);
+            var newVersion = GetVersionFromPackage(newPackagePath);
+            var workDir = Path.Combine(workingDirectory, $"{packageName}.Diff.{oldVersion}-{newVersion}");
+            if (Directory.Exists(workDir))
+                DeleteEmptyDirs(workDir);
+
+            CopyDirectory(newPackagePath.Replace(".zip", ""), workDir, oldPackagePath.Replace(".zip", ""));
+
+            _zipService.CreateZip(workDir + ".zip", workDir);
+            DeleteEmptyDirs(workDir);
+
+        }
+
+        private static string GetVersionFromPackage(string packagePath)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(packagePath);
+            var versionPart = fileName.Substring(packageNameVersionIndexOf.Length);
+            return versionPart;
+        }
+
+        private static void Extract(string packagePath)
+        {
+            if (File.Exists(packagePath))
+            {
+                var extractDir = packagePath.Replace(".zip", "");
+                if (Directory.Exists(extractDir) == false)
+                    _zipService.ExtractZipFile(packagePath, packagePath.Replace(".zip", ""));
+            }
+        }
+
+
+        /// <summary>
+        /// A performance-optimized method to copy a directory to another including all subdirectories
+        /// Adapted from: http://stackoverflow.com/a/2801234/5018
+        /// </summary>
+        /// <param name="source">The source directory</param>
+        /// <param name="target">The target to copy to</param>
+        private static bool CopyDirectory(string source, string target, string olddir)
+        {
+            try
+            {
+                var stack = new Stack<Folders>();
+                stack.Push(new Folders(source, target));
+
+                while (stack.Count > 0)
+                {
+                    var folders = stack.Pop();
+                    if (Directory.Exists(folders.Target) == false)
+                        Directory.CreateDirectory(folders.Target);
+
+                    if (Directory.Exists(folders.Source) == false)
+                        Directory.CreateDirectory(folders.Source);
+
+                    foreach (var file in Directory.GetFiles(folders.Source, "*.*"))
+                    {
+                        if (file == null)
+                            continue;
+
+                        // if the file is new compared to the old version, copy it
+                        var oldFilePath = file.Replace(source, olddir);
+                        if (File.Exists(oldFilePath))
+                        {
+                            var oldfileInfo = new FileInfo(oldFilePath);
+                            var newFileInfo = new FileInfo(file);
+                            if (oldfileInfo.Length == newFileInfo.Length)
+                            {
+                                if (FileEquals(oldFilePath, file))
+                                    continue;
+                            }
+                        }
+
+                        var targetFile = Path.Combine(folders.Target, Path.GetFileName(file));
+
+
+
+                        File.Copy(file, targetFile, true);
+                    }
+
+                    foreach (var folder in Directory.GetDirectories(folders.Source))
+                    {
+                        if (folder != null)
+                            stack.Push(new Folders(folder, Path.Combine(folders.Target, Path.GetFileName(folder))));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+        internal class Folders
+        {
+            internal string Source { get; private set; }
+            internal string Target { get; private set; }
+
+            internal Folders(string source, string target)
+            {
+                Source = source;
+                Target = target;
+            }
+        }
+        static bool FileEquals(string path1, string path2)
+        {
+            byte[] file1 = File.ReadAllBytes(path1);
+            byte[] file2 = File.ReadAllBytes(path2);
+            if (file1.Length == file2.Length)
+            {
+                for (int i = 0; i < file1.Length; i++)
+                {
+                    if (file1[i] != file2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        static void DeleteEmptyDirs(string dir)
+        {
+            if (String.IsNullOrEmpty(dir))
+                throw new ArgumentException(
+                    "Starting directory is a null reference or an empty string",
+                    "dir");
+
+            try
+            {
+                foreach (var d in Directory.EnumerateDirectories(dir))
+                {
+                    DeleteEmptyDirs(d);
+                }
+
+                var entries = Directory.EnumerateFileSystemEntries(dir);
+
+                if (!entries.Any())
+                {
+                    try
+                    {
+                        Directory.Delete(dir);
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (DirectoryNotFoundException) { }
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+        }
+    }
+}
---
+++ ExtractDiff/ZipService.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace ExtractDiff
+{
+    public class ZipService
+    {
+
+        /// <summary>
+        /// Extracts an zip file to a specified output folder using the ICSharpCode.SharpZipLib 
+        /// NB. Empty folders in the zip aren't extracted
+        /// </summary>
+        /// <param name="archiveFilenameIn">The zip file to extract</param>
+        /// <param name="outFolder">The folder to put the file in. The folder is created if it doesnt exist</param>
+        public bool ExtractZipFile(string archiveFilenameIn, string outFolder)
+        {
+            try
+            {
+                ZipFile zipFile = null;
+                try
+                {
+                    var fileStream = File.OpenRead(archiveFilenameIn);
+                    zipFile = new ZipFile(fileStream);
+                    foreach (ZipEntry zipEntry in zipFile)
+                    {
+                        // Ignore directories
+                        if (zipEntry.IsFile == false)
+                            continue;
+
+                        var entryFileName = zipEntry.Name;
+
+                        var buffer = new byte[2048];
+                        var zipStream = zipFile.GetInputStream(zipEntry);
+
+                        var fullZipToPath = Path.Combine(outFolder, entryFileName);
+                        var directoryName = Path.GetDirectoryName(fullZipToPath);
+
+                        if (string.IsNullOrEmpty(directoryName) == false)
+                            Directory.CreateDirectory(directoryName);
+
+                        // Unzip file in buffered chunks. This is just as fast as unpacking to a buffer the full size
+                        // of the file, but does not waste memory.
+                        // The "using" will close the stream even if an exception occurs.
+                        using (var streamWriter = File.Create(fullZipToPath))
+                        {
+                            StreamUtils.Copy(zipStream, streamWriter, buffer);
+                        }
+                    }
+                }
+                finally
+                {
+                    if (zipFile != null)
+                    {
+                        zipFile.IsStreamOwner = true; // Makes close also shut the underlying stream
+                        zipFile.Close(); // Ensure we release resources
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        // Compresses the files in the nominated folder, and creates a zip file on disk named as outPathname.
+        //
+        public void CreateZip(string outPathname, string folderName)
+        {
+            FileStream fsOut = File.Create(outPathname);
+            ZipOutputStream zipStream = new ZipOutputStream(fsOut);
+
+            zipStream.SetLevel(3); //0-9, 9 being the highest level of compression
+
+            // This setting will strip the leading part of the folder path in the entries, to
+            // make the entries relative to the starting folder.
+            // To include the full path for each entry up to the drive root, assign folderOffset = 0.
+            int folderOffset = folderName.Length + (folderName.EndsWith("\\") ? 0 : 1);
+
+            CompressFolder(folderName, zipStream, folderOffset);
+
+            zipStream.IsStreamOwner = true; // Makes the Close also Close the underlying stream
+            zipStream.Close();
+        }
+
+        // Recurses down the folder structure
+        //
+        private void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset)
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string filename in files)
+            {
+                FileInfo fi = new FileInfo(filename);
+
+                string entryName = filename.Substring(folderOffset); // Makes the name in zip based on the folder
+                entryName = ZipEntry.CleanName(entryName); // Removes drive from name and fixes slash direction
+                ZipEntry newEntry = new ZipEntry(entryName);
+                newEntry.DateTime = fi.LastWriteTime; // Note the zip format stores 2 second granularity
+
+                newEntry.Size = fi.Length;
+
+                zipStream.PutNextEntry(newEntry);
+
+                // Zip the file in buffered chunks
+                // the "using" will close the stream even if an exception occurs
+                byte[] buffer = new byte[4096];
+                using (FileStream streamReader = File.OpenRead(filename))
+                {
+                    StreamUtils.Copy(streamReader, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+            string[] folders = Directory.GetDirectories(path);
+            foreach (string folder in folders)
+            {
+                CompressFolder(folder, zipStream, folderOffset);
+            }
+        }
+    }
+}
